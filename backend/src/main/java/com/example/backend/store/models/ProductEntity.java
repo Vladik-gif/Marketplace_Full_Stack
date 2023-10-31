c4e2beef6304e0d0fb454ef51df57d634ca324b2
Vladik <v4054741@gmail.com>
2023-10-31 10:42:17 +0300

    Product entity project

---
+++ backend/src/main/java/com/example/backend/store/models/ProductEntity.java
@@ -0,0 +1,79 @@
+package com.example.backend.store.models;
+
+import jakarta.persistence.*;
+import java.awt.*;
+
+@Entity
+@Table(name = "Product")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description_product;
+    private String characteristic_product;
+    private double price;
+    //private String customer_reviews;
+    private String seller;
+    //private Image image;
+    private int number;
+
+    public ProductEntity() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription_product() {
+        return description_product;
+    }
+
+    public void setDescription_product(String description_product) {
+        this.description_product = description_product;
+    }
+
+    public String getCharacteristic_product() {
+        return characteristic_product;
+    }
+
+    public void setCharacteristic_product(String characteristic_product) {
+        this.characteristic_product = characteristic_product;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
