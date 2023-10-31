a24d5455444d8696e974b0cb8e7e2d3bb32823f3
Vladik <v4054741@gmail.com>
2023-10-31 10:44:00 +0300

    ProductFactory,connection with the server

---
+++ backend/src/main/java/com/example/backend/api/factory/ProductFactory.java
@@ -0,0 +1,22 @@
+package com.example.backend.api.factory;
+
+import com.example.backend.api.DTO.ProductDTO;
+import com.example.backend.store.models.ProductEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductFactory {
+
+    public ProductDTO makeProduct(ProductEntity entity){
+        ProductDTO productDTO = new ProductDTO();
+
+        productDTO.setId(entity.getId());
+        productDTO.setName(entity.getName());
+        productDTO.setSeller(entity.getSeller());
+        productDTO.setNumber(entity.getNumber());
+        productDTO.setCharacteristic_product(entity.getCharacteristic_product());
+        productDTO.setDescription_product(entity.getDescription_product());
+
+        return productDTO;
+    }
+}
