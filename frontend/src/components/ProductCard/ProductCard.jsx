aaf81dc2b76bbd646b2ca2ed8db2cb595730e748
Nazar <npogrebecikyj@gmail.com>
2023-12-02 19:59:58 +0200

    it work but without db

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -3,13 +3,23 @@ import styles from './ProductCard.module.css';
 import TestGoods from '../../assets/imges/TestGoods.jpg';
 
 const ProductCard = () => {
-  return (
-    <div className={styles.product_card}>
-      <img src={TestGoods} alt="goods" className={styles.img_product} />
-      <span className={styles.name_product}>Назва</span>
-      <br />
-      <span className={styles.price_product}>Ціна</span>
-    </div>
-  );
+   const takeDate = () => {
+      fetch('http://localhost:8080/get/products')
+         .then((response) => {
+            return response.json();
+         })
+         .then((data) => {
+            console.log(data);
+         });
+   }
+
+   return (
+      <div className={styles.product_card} onClick={takeDate}>
+         <img src={TestGoods} alt="goods" className={styles.img_product} />
+         <span className={styles.name_product}>Назва</span>
+         <br />
+         <span className={styles.price_product}>Ціна</span>
+      </div>
+   );
 };
 export default ProductCard;
