c4f4f12f4d38deddf40e815525e9fd9b2f1d4d5e
Nazar <npogrebecikyj@gmail.com>
2023-12-06 18:32:08 +0200

    test add date to bd

--- frontend/src/components/MainLayout/MainLayout.jsx
+++ frontend/src/components/MainLayout/MainLayout.jsx
@@ -3,19 +3,21 @@ import { Outlet } from 'react-router-dom';
 
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import FormTest from '../FormForTest/FormTest';
 
 // import styles from './MainLayout.module.css';
 
 const MainLayout = () => {
-  return (
-    <div>
-      <Header />
-      <Suspense>
-        <Outlet />
-      </Suspense>
-      <Footer />
-    </div>
-  );
+   return (
+      <div>
+         <Header />
+         <Suspense>
+            <Outlet />
+         </Suspense>
+         <FormTest />
+         <Footer />
+      </div>
+   );
 };
 
 export default MainLayout;
