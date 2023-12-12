220e184488d2810c051d1ccc465e1339dc59f392
Bohdan Bortnyk <bohdan0211@gmail.com>
2023-12-12 20:59:07 +0200

    done style card

--- frontend/src/pages/SecondPage/SecondPage.jsx
+++ frontend/src/pages/SecondPage/SecondPage.jsx
@@ -337,6 +337,343 @@ const SecondCPage = () => {
         ]);
         break;
 
+      case '/tablet':
+        setTitle('Планшети та електронні книги');
+        setDataToShow([
+          {
+            id: 59,
+            name: 'Дитячі планшети',
+            image: 'https://loremflickr.com/320/300',
+          },
+          {
+            id: 60,
+            name: 'Графічні планшети',
+            image: 'https://loremflickr.com/320/301',
+          },
+
+          {
+            id: 61,
+            name: 'Бюджетні планшети',
+            image: 'https://loremflickr.com/320/302',
+          },
+          {
+            id: 62,
+            name: 'Планшети інших категорій',
+            image: 'https://loremflickr.com/320/303',
+          },
+          {
+            id: 63,
+            name: 'Стилуси',
+            image: 'https://loremflickr.com/320/304',
+          },
+          {
+            id: 64,
+            name: 'Чохли для планшетів',
+            image: 'https://loremflickr.com/320/305',
+          },
+          {
+            id: 65,
+            name: 'Захистне скло',
+            image: 'https://loremflickr.com/320/306',
+          },
+          {
+            id: 66,
+            name: 'Аксесуари',
+            image: 'https://loremflickr.com/320/307',
+          },
+          {
+            id: 67,
+            name: 'Електронні книги',
+            image: 'https://loremflickr.com/320/308',
+          },
+          {
+            id: 68,
+            name: 'Аксесуари для електронних книг',
+            image: 'https://loremflickr.com/320/309',
+          },
+        ]);
+        break;
+
+      case '/laptop':
+        setTitle('Ноутбуки та аксесуари');
+        setDataToShow([
+          {
+            id: 70,
+            name: 'Ноутбуки для роботи',
+            image: 'https://loremflickr.com/320/310',
+          },
+          {
+            id: 71,
+            name: 'Ноутбуки для навчання',
+            image: 'https://loremflickr.com/320/311',
+          },
+
+          {
+            id: 72,
+            name: 'Маленькі ноутбуки',
+            image: 'https://loremflickr.com/320/312',
+          },
+          {
+            id: 73,
+            name: 'Ноутбуки з сенсорним екраном',
+            image: 'https://loremflickr.com/320/313',
+          },
+          {
+            id: 74,
+            name: 'Іншу ноутбуки',
+            image: 'https://loremflickr.com/320/314',
+          },
+          {
+            id: 75,
+            name: 'Аксесуари ',
+            image: 'https://loremflickr.com/320/315',
+          },
+          {
+            id: 76,
+            name: 'Підставки',
+            image: 'https://loremflickr.com/320/316',
+          },
+          {
+            id: 77,
+            name: 'Акумулятори',
+            image: 'https://loremflickr.com/320/317',
+          },
+          {
+            id: 78,
+            name: 'Зарядні присторої',
+            image: 'https://loremflickr.com/320/318',
+          },
+          {
+            id: 79,
+            name: 'Блоки живлення',
+            image: 'https://loremflickr.com/320/319',
+          },
+        ]);
+        break;
+
+      case '/kitchen':
+        setTitle('Техніка для кухні');
+        setDataToShow([
+          {
+            id: 80,
+            name: 'Холодильники та морозильники',
+            image: 'https://loremflickr.com/320/320',
+          },
+          {
+            id: 81,
+            name: 'Посудомийні машини',
+            image: 'https://loremflickr.com/320/321',
+          },
+
+          {
+            id: 82,
+            name: 'Електрогрилі, печі, духовки',
+            image: 'https://loremflickr.com/320/322',
+          },
+          {
+            id: 83,
+            name: 'Мультиварки',
+            image: 'https://loremflickr.com/320/323',
+          },
+          {
+            id: 84,
+            name: 'Комбайни, міксери, м’ясорубки',
+            image: 'https://loremflickr.com/320/324',
+          },
+          {
+            id: 85,
+            name: 'Кавомашині, кавомолки',
+            image: 'https://loremflickr.com/320/325',
+          },
+          {
+            id: 86,
+            name: 'Електроплити, поверхні та витяжки',
+            image: 'https://loremflickr.com/320/326',
+          },
+          {
+            id: 87,
+            name: 'Соковижималки',
+            image: 'https://loremflickr.com/320/327',
+          },
+          {
+            id: 88,
+            name: 'Електрочайники',
+            image: 'https://loremflickr.com/320/328',
+          },
+          {
+            id: 89,
+            name: 'Мікрохвильові печі',
+            image: 'https://loremflickr.com/320/329',
+          },
+        ]);
+        break;
+
+      case '/home':
+        setTitle('Техніка для дому');
+        setDataToShow([
+          {
+            id: 90,
+            name: 'Електрокаміни',
+            image: 'https://loremflickr.com/320/317',
+          },
+          {
+            id: 91,
+            name: 'Пральні машини',
+            image: 'https://loremflickr.com/320/336',
+          },
+
+          {
+            id: 92,
+            name: 'Водонагрівачі',
+            image: 'https://loremflickr.com/320/322',
+          },
+          {
+            id: 93,
+            name: 'Пилососи, роботи-пилососи',
+            image: 'https://loremflickr.com/320/333',
+          },
+          {
+            id: 94,
+            name: 'Пароочистники',
+            image: 'https://loremflickr.com/320/331',
+          },
+          {
+            id: 95,
+            name: 'Праски, відпарювачі',
+            image: 'https://loremflickr.com/320/317',
+          },
+          {
+            id: 96,
+            name: 'Швейна техніка',
+            image: 'https://loremflickr.com/320/312',
+          },
+          {
+            id: 97,
+            name: 'Фени, прилади для укладення волосся',
+            image: 'https://loremflickr.com/320/338',
+          },
+          {
+            id: 98,
+            name: 'Електромасажери',
+            image: 'https://loremflickr.com/320/334',
+          },
+          {
+            id: 99,
+            name: 'Дрібна побутова техніка',
+            image: 'https://loremflickr.com/320/333',
+          },
+        ]);
+        break;
+
+      case '/climate':
+        setTitle('Кліматичне обладнання');
+        setDataToShow([
+          {
+            id: 100,
+            name: 'Обігрівачі',
+            image: 'https://loremflickr.com/320/340',
+          },
+          {
+            id: 101,
+            name: 'Кліматичні комплекси',
+            image: 'https://loremflickr.com/320/341',
+          },
+
+          {
+            id: 102,
+            name: 'Очисники повітря',
+            image: 'https://loremflickr.com/320/342',
+          },
+          {
+            id: 103,
+            name: 'Зволожувачі повітря',
+            image: 'https://loremflickr.com/320/343',
+          },
+          {
+            id: 104,
+            name: 'Осушувачі повітря',
+            image: 'https://loremflickr.com/320/344',
+          },
+          {
+            id: 105,
+            name: 'Кондиціонери',
+            image: 'https://loremflickr.com/320/345',
+          },
+          {
+            id: 106,
+            name: 'Вентилятори',
+            image: 'https://loremflickr.com/320/346',
+          },
+          {
+            id: 107,
+            name: 'Котли опалення',
+            image: 'https://loremflickr.com/320/347',
+          },
+          {
+            id: 108,
+            name: 'Запчастини та аксесуари',
+            image: 'https://loremflickr.com/320/348',
+          },
+        ]);
+        break;
+
+      case '/other':
+        setTitle('Зарядні станції');
+        setDataToShow([
+          {
+            id: 110,
+            name: 'Портативні зарядні станції',
+            image: 'https://loremflickr.com/320/351',
+          },
+          {
+            id: 111,
+            name: 'З.с. від сонячних батарей',
+            image: 'https://loremflickr.com/320/351',
+          },
+
+          {
+            id: 112,
+            name: 'З.с. від розетки',
+            image: 'https://loremflickr.com/320/352',
+          },
+          {
+            id: 113,
+            name: 'Додаткові батареї',
+            image: 'https://loremflickr.com/320/353',
+          },
+          {
+            id: 114,
+            name: 'Комплекти енергонезалежності',
+            image: 'https://loremflickr.com/320/354',
+          },
+          {
+            id: 115,
+            name: 'З.с. автомобільний адаптер',
+            image: 'https://loremflickr.com/320/355',
+          },
+          {
+            id: 116,
+            name: 'Павербенки',
+            image: 'https://loremflickr.com/320/356',
+          },
+          {
+            id: 117,
+            name: 'Сонячні панелі для зарядних станцій',
+            image: 'https://loremflickr.com/320/357',
+          },
+          {
+            id: 118,
+            name: 'Генератори',
+            image: 'https://loremflickr.com/320/358',
+          },
+          {
+            id: 119,
+            name: 'Подовжувачі',
+            image: 'https://loremflickr.com/320/359',
+          },
+        ]);
+        break;
+
       default:
         setTitle('');
         setDataToShow([]);
