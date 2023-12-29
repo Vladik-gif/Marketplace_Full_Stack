899aa679f07270ab22cd2bbd55d726ebc7ddf71b
Bohdan Bortnyk <bohdan0211@gmail.com>
2023-12-29 11:28:33 +0200

    add filter

--- frontend/src/pages/ThirdPage/ThirdPage.jsx
+++ frontend/src/pages/ThirdPage/ThirdPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import ProductCardList from '../../components/ProductCardList/ProductCardList';
 import styles from './ThirdPage.module.css';
 
@@ -9,6 +10,8 @@ const db = [
     discription: 'Samsung Galaxy S21 FE G990B 6/128GB Light Violet (SM-G990BLVFSEK) NEW',
     price: 20000,
     image: 'https://loremflickr.com/320/500',
+    color: 'red',
+    camera: 2,
   },
   {
     id: 2,
@@ -17,6 +20,8 @@ const db = [
     discription: 'Samsung Galaxy S21 FE G990B 6/128GB Light Violet (SM-G990BLVFSEK) NEW',
     price: 10000,
     image: 'https://loremflickr.com/320/501',
+    color: 'blue',
+    camera: 1,
   },
   {
     id: 3,
@@ -25,6 +30,8 @@ const db = [
     discription: 'Samsung Galaxy S21 FE G990B 6/128GB Light Violet (SM-G990BLVFSEK) NEW',
     price: 30000,
     image: 'https://loremflickr.com/320/502',
+    color: 'white',
+    camera: 3,
   },
   {
     id: 4,
@@ -33,13 +40,40 @@ const db = [
     discription: 'Samsung Galaxy S21 FE G990B 6/128GB Light Violet (SM-G990BLVFSEK) NEW',
     price: 20000,
     image: 'https://loremflickr.com/320/503',
+    color: 'black',
+    camera: 2,
   },
 ];
 
 const ThirdPage = () => {
   return (
     <div className={styles.thirdPageContainer}>
-      <div className={styles.filterNav}></div>
+      <div className={styles.filterNav}>
+        <form className={styles.filterForm} id="filter-form">
+          <label>
+            Від: <input type="number" min="0" />
+          </label>
+
+          <label>
+            До: <input type="number" min="0" />
+          </label>
+
+          <label for="color">Колір: </label>
+          <input type="checkbox" value="red" />
+
+          <label for="brand">Бренд: </label>
+          <select id="brand">
+            <option value="">Усі</option>
+            <option value="nike">Nike</option>
+            <option value="adidas">Adidas</option>
+            <option value="puma">Puma</option>
+          </select>
+
+          <button type="button" onclick="filterProducts()">
+            Фільтрувати
+          </button>
+        </form>
+      </div>
       <div className={styles.productCard}>
         <ProductCardList db={db} />
       </div>
