8f05b21ed7cf8699fd3bac3c4baaedb1e70f0525
Bohdan Bortnyk <bohdan0211@gmail.com>
2024-01-04 18:22:05 +0200

    fix filter smartphone

--- frontend/src/components/Filter/Mobile/FilterSmartphone/FilterSmartphone.jsx
+++ frontend/src/components/Filter/Mobile/FilterSmartphone/FilterSmartphone.jsx
@@ -1,33 +1,11 @@
 import styles from './FilterSmartphone.module.css';
 
-const FilterSmartphone = db => {
-  const data = db.filter('Телефони');
-  console.log(data);
-
+const FilterSmartphone = () => {
   return (
     <>
-      <form className={styles.filterForm} id="filter-form">
-        <label>
-          Від:
-          <input type="number" min="0" />
-        </label>
-
-        <label>
-          До:
-          <input type="number" min="0" />
-        </label>
-
-        <p>Колір:</p>
-
-        {db.map(({ id, color }) => (
-          <label key={id}>
-            <input type="checkbox" />
-            {color}
-          </label>
-        ))}
-
-        <button type="button">Фільтрувати</button>
-      </form>
+      <>
+        <p>Smartphone</p>
+      </>
     </>
   );
 };
