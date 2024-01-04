e0e6e5c67071b35dfe6a4de30c811d84fe311ac6
Bohdan Bortnyk <bohdan0211@gmail.com>
2024-01-04 18:39:59 +0200

    fix bug

--- frontend/src/components/ProductCardList/ProductCardList.jsx
+++ frontend/src/components/ProductCardList/ProductCardList.jsx
@@ -1,4 +1,5 @@
 import ProductCardListItem from '../ProductCardListItem/ProductCardListItem';
+import PropTypes from 'prop-types';
 
 import styles from './ProductCardList.module.css';
 
@@ -19,3 +20,14 @@ const ProductCardList = ({ db }) => {
 };
 
 export default ProductCardList;
+
+ProductCardList.propTypes = {
+  db: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      discription: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
--- frontend/src/components/ProductCardListItem/ProductCardListItem.jsx
+++ frontend/src/components/ProductCardListItem/ProductCardListItem.jsx
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom';
 import SvgIcon from '@mui/material/SvgIcon';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import BalanceOutlinedIcon from '@mui/icons-material/BalanceOutlined';
+import PropTypes from 'prop-types';
 
 import styles from './ProductCardListItem.module.css';
 
@@ -10,7 +11,7 @@ const ProductCardListItem = ({ image, discription, price }) => {
     <>
       <div className={styles.prodCardItem}>
         <div className={styles.prodCardItem__wraper}>
-          <img className={styles.prodCardItem__img} src={image} />
+          <img className={styles.prodCardItem__img} src={image} alt={'electronics'} />
         </div>
 
         <div className={styles.prodCardItem__content}>
@@ -18,14 +19,16 @@ const ProductCardListItem = ({ image, discription, price }) => {
           <p className={styles.prodCardItem__price}>{price} ₴</p>
         </div>
         <div className={styles.prodCardItem__nav}>
-          <NavLink className={styles.prodCardItem__btn}>ПЕРЕГЛЯНУТИ</NavLink>
+          <NavLink to={'/'} className={styles.prodCardItem__btn}>
+            ПЕРЕГЛЯНУТИ
+          </NavLink>
           <div className={styles.prodCardItem__linkIcon}>
-            <NavLink className={styles.prodCardItem__basket}>
+            <NavLink to={'/'} className={styles.prodCardItem__basket}>
               <SvgIcon sx={{ width: 30, height: 30 }}>
                 <ShoppingCartOutlinedIcon />
               </SvgIcon>
             </NavLink>
-            <NavLink className={styles.prodCardItem__balance}>
+            <NavLink to={'/'} className={styles.prodCardItem__balance}>
               <SvgIcon sx={{ width: 30, height: 30 }}>
                 <BalanceOutlinedIcon />
               </SvgIcon>
@@ -38,3 +41,9 @@ const ProductCardListItem = ({ image, discription, price }) => {
 };
 
 export default ProductCardListItem;
+
+ProductCardListItem.propTypes = {
+  image: PropTypes.string.isRequired,
+  discription: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
